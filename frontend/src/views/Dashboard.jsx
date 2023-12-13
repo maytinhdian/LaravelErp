357171805d3abd2b11e8dfd032f99b3c7d160f7a
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-12-14 05:18:28 +0700

    Create dashboard

--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -1,135 +1,202 @@
-import PageComponent from "../components/PageComponent";
-import DashboardCard from "../components/DashboardCard.jsx";
-import { useEffect, useState } from "react";
-import axiosClient from "../axios.js";
-import TButton from "../components/core/TButton.jsx";
-import { EyeIcon, PencilIcon } from "@heroicons/react/24/outline";
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+  { name: 'Reports', href: '#', current: false },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Dashboard() {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    setLoading(true);
-    axiosClient
-      .get(`/dashboard`)
-      .then((res) => {
-        setLoading(false);
-        setData(res.data);
-        return res;
-      })
-      .catch((error) => {
-        setLoading(false);
-        return error;
-      });
-  }, []);
-
   return (
-    <PageComponent title="Dashboard">
-      {loading && <div className="flex justify-center">Loading...</div>}
-      {!loading && (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 text-gray-700">
-          <DashboardCard
-            title="Total Surveys"
-            className="order-1 lg:order-2"
-            style={{ animationDelay: '0.1s' }}
-          >
-            <div className="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center">
-              {data.totalSurveys}
-            </div>
-          </DashboardCard>
-          <DashboardCard
-            title="Total Answers"
-            className="order-2 lg:order-4"
-            style={{ animationDelay: '0.2s' }}
-          >
-            <div className="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center">
-              {data.totalAnswers}
-            </div>
-          </DashboardCard>
-          <DashboardCard
-            title="Latest Survey"
-            className="order-3 lg:order-1 row-span-2"
-            style={{ animationDelay: '0.2s' }}
-          >
-            {data.latestSurvey && (
-              <div>
-                <img
-                  src={data.latestSurvey.image_url}
-                  className="w-[240px] mx-auto"
-                />
-                <h3 className="font-bold text-xl mb-3">
-                  {data.latestSurvey.title}
-                </h3>
-                <div className="flex justify-between text-sm mb-1">
-                  <div>Create Date:</div>
-                  <div>{data.latestSurvey.created_at}</div>
-                </div>
-                <div className="flex justify-between text-sm mb-1">
-                  <div>Expire Date:</div>
-                  <div>{data.latestSurvey.expire_date}</div>
-                </div>
-                <div className="flex justify-between text-sm mb-1">
-                  <div>Status:</div>
-                  <div>{data.latestSurvey.status ? "Active" : "Draft"}</div>
-                </div>
-                <div className="flex justify-between text-sm mb-1">
-                  <div>Questions:</div>
-                  <div>{data.latestSurvey.questions}</div>
-                </div>
-                <div className="flex justify-between text-sm mb-3">
-                  <div>Answers:</div>
-                  <div>{data.latestSurvey.answers}</div>
-                </div>
-                <div className="flex justify-between">
-                  <TButton to={`/surveys/${data.latestSurvey.id}`} link>
-                    <PencilIcon className="w-5 h-5 mr-2" />
-                    Edit Survey
-                  </TButton>
+    <>
+      {/*
+        This example requires updating your template:
 
-                  <TButton link>
-                    <EyeIcon className="w-5 h-5 mr-2" />
-                    View Answers
-                  </TButton>
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-8 w-8"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <div className="hidden md:block">
+                      <div className="ml-10 flex items-baseline space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                              'rounded-md px-3 py-2 text-sm font-medium'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hidden md:block">
+                    <div className="ml-4 flex items-center md:ml-6">
+                      <button
+                        type="button"
+                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      >
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+
+                      {/* Profile dropdown */}
+                      <Menu as="div" className="relative ml-3">
+                        <div>
+                          <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">Open user menu</span>
+                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                          </Menu.Button>
+                        </div>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            {userNavigation.map((item) => (
+                              <Menu.Item key={item.name}>
+                                {({ active }) => (
+                                  <a
+                                    href={item.href}
+                                    className={classNames(
+                                      active ? 'bg-gray-100' : '',
+                                      'block px-4 py-2 text-sm text-gray-700'
+                                    )}
+                                  >
+                                    {item.name}
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            ))}
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                  </div>
+                  <div className="-mr-2 flex md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-0.5" />
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Disclosure.Button>
+                  </div>
                 </div>
               </div>
-            )}
-            {!data.latestSurvey && (
-              <div className="text-gray-600 text-center py-16">
-                Your don't&apos have surveys yet
-              </div>
-            )}
-          </DashboardCard>
-          <DashboardCard
-            title="Latest Answers"
-            className="order-4 lg:order-3 row-span-2"
-            style={{ animationDelay: '0.3s' }}
-          >
-            {data.latestAnswers.length && (
-              <div className="text-left">
-                {data.latestAnswers.map((answer) => (
-                  <a
-                    href="#"
-                    key={answer.id}
-                    className="block p-2 hover:bg-gray-100/90"
-                  >
-                    <div className="font-semibold">{answer.survey.title}</div>
-                    <small>
-                      Answer Made at:
-                      <i className="font-semibold">{answer.end_date}</i>
-                    </small>
-                  </a>
-                ))}
-              </div>
-            )}
-            {!data.latestAnswers.length && (
-              <div className="text-gray-600 text-center py-16">
-                Your don't have answers yet
-              </div>
-            )}
-          </DashboardCard>
-        </div>
-      )}
-    </PageComponent>
-  );
+
+              <Disclosure.Panel className="md:hidden">
+                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                  {navigation.map((item) => (
+                    <Disclosure.Button
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block rounded-md px-3 py-2 text-base font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  ))}
+                </div>
+                <div className="border-t border-gray-700 pb-3 pt-4">
+                  <div className="flex items-center px-5">
+                    <div className="flex-shrink-0">
+                      <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium leading-none text-white">{user.name}</div>
+                      <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                    </div>
+                    <button
+                      type="button"
+                      className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    >
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="mt-3 space-y-1 px-2">
+                    {userNavigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+        </main>
+      </div>
+    </>
+  )
 }
