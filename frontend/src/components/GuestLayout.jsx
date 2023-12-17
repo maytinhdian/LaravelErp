88d4c1a26833706f78e8f012c701463e079f406f
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-12-18 05:36:46 +0700

    Improve Login Signup Page

--- frontend/src/components/GuestLayout.jsx
+++ frontend/src/components/GuestLayout.jsx
@@ -2,24 +2,24 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
-    const {currentUser,userToken}=useStateContext();
+    const { userToken } = useStateContext();
 
     if (userToken) {
-      return <Navigate to="/dashboard"/>
+        return <Navigate to="/" />;
     }
 
-  return (
-    <>
-      <div className="flex  min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="/src/assets/tmt_logo.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-        </div>
-        <Outlet />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex  min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="src/assets/tmt_logo.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                    />
+                </div>
+                <Outlet />
+            </div>
+        </>
+    );
 }
