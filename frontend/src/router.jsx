dbc4879e68a14edab03a50ac225b64c76cd2fb54
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-12-19 08:47:32 +0700

    Create Product Component

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -6,7 +6,6 @@ import DefaultLayout from "./components/DefaultLayout";
 import GuestLayout from "./components/GuestLayout";
 import Signup from "./views/signup";
 import Product from "./views/product";
-import Test from "./views/test";
 
 const router = createBrowserRouter([
 
